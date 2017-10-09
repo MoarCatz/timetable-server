5b97b1c227b61c157290b4d044983cf2fba3c34a
Leva7 <leva181777@gmail.com>
2017-10-09 22:50:04 +0500

    :bug: Allows non-ASCII characters to be included in JSON

--- encoder.py
+++ encoder.py
@@ -4,4 +4,5 @@ class NoWSEncoder(json.JSONEncoder):
     '''Subclass to eliminate whitespace in the resulting JSON'''
     def __init__(self, **kwargs):
         kwargs['separators'] = (',', ':')
+        kwargs['ensure_ascii'] = False
         super().__init__(**kwargs)
